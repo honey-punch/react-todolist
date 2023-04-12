5548348671139f0e3f801a71dce0554733ad376a
mad <mad@jangjicheol.local>
2023-04-12 13:42:53 +0900

    favicon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,4 @@
-import React from 'react';
 import 'css/header.css';
-import logo from 'logo.png'
-
 
 type Props = {
     toggleDialog: () => void;
@@ -10,7 +7,7 @@ type Props = {
 export default function Header(props: Props) {
     return (
         <header className='header'>
-            <h1 className='header__logo'><img src={logo} alt="logo" /></h1>
+            <h1 className='header__logo'><img src={process.env.PUBLIC_URL + '/img/logo.png'} alt="logo" /></h1>
             <button className='header__btn' onClick={props.toggleDialog} type='button'>
                 <i className="fa-solid fa-circle-plus"></i>
             </button>
